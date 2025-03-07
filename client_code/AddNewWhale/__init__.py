89b6c552fa5334b9bc0397eb069c3d49ec9d1830
harrygorden@gmail.com <harrygorden@gmail.com>
2025-03-07 03:32:24 +0000

    Created WhaleWatcher, AddNewWhale and WhaleWatcher.RowTemplate1; Edited settings; Deleted Form1

--- client_code/AddNewWhale/__init__.py
+++ client_code/AddNewWhale/__init__.py
@@ -1,8 +1,9 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import AddNewWhaleTemplate
 from anvil import *
+import anvil.server
 
 
-class Form1(Form1Template):
+class AddNewWhale(AddNewWhaleTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
